559815f86162f3d588164c2962b16a04029908fa
ekanandaputra <52701387+ekanandaputra@users.noreply.github.com>
2019-07-09 19:50:35 +0700

    Create PenerimaSerial.ino

---
+++ Komunikasi-Serial/PenerimaSerial.ino
@@ -0,0 +1,23 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial data_serial(3,4);
+String dt[10];
+String dataIn;
+int i=0;
+
+void setup() {
+  Serial.begin(9600);
+  data_serial.begin(9600);
+}
+
+void loop() {
+    dataIn="";
+    while(data_serial.available()>0){
+      delay(10);
+      char c = data_serial.read();
+      dataIn = dataIn + c;    
+    }
+    if(dataIn.length()>0){
+      Serial.println(dataIn);
+    }
+}
